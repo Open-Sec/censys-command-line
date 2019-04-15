59b331637da7402e74f3e3240e37bfeb0cd90ee1
asturdevant <asturdevant@asturdevants-MacBook-Pro.local>
2019-04-15 13:16:28 -0400

    Moved version up a little to reflect new command line name.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setuptools.setup(
     name="censys_command_line",
-    version="1.0.0",
+    version="1.0.1",
     author="Art Sturdevant",
     author_email="support@censys.io",
     description="Easily query the Censys API from the command line",
